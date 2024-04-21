6abe91dcabc8b2fcd01b6b8e423045731a089066
EdgarDaza <edgar.daza@utp.ac.pa>
2024-04-21 18:14:26 -0500

    Primeros archivos en subir

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Doctor! Ready for our next adventure?");
+        JOptionPane.showMessageDialog(null, "So... all of time and space, everything that happened or ever will -" +
+                " Where do you want to start?");
+
+    }
+}
